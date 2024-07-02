a485df423063761830a267ae641f473a058524df
Gil Kagan <gilkag2@gmail.com>
2024-07-02 16:24:05 +0300

    update

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
+import axios from "axios";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-// Middleware to parse JSON bodies
-// app.use(bodyParser.json({}));
+app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const conversionMap = new Map();
@@ -15,15 +15,10 @@ const conversionMap = new Map();
 conversionMap.set("1", 50);
 conversionMap.set("2", 50);
 
-// Webhook endpoint
-app.post("/webhook", async (req, res) => {
-  console.log("recieved data:");
-  console.log(req.body);
-
+app.post("/", async (req, res) => {
   try {
-    processData(req.body);
-    // const transformedData = processPaymentData(webhookData);
-    // await createInvoice(webhookData);
+    const newInvoiceData = processData(req.body);
+    await createInvoice(newInvoiceData);
 
     res.status(200).send("Webhook received and invoice created successfully");
   } catch (error) {
@@ -34,54 +29,77 @@ app.post("/webhook", async (req, res) => {
 
 const processData = (data) => {
   console.log(`processing data for item ${data["ProdItemID"]}`);
-  const priceWithoutVat = conversionMap.get(data["ProdItemID"]); // maps product name to vat pricing information
-  if (!priceWithoutVat) {
+  const commissionPrice = conversionMap.get(data["ProdItemID"]); // maps product name to vat pricing information
+  if (!commissionPrice) {
+    console.log("Product ID not found in conversion map.");
     return;
   }
-  const priceWithVat = +data["ProdPrice"] - priceWithoutVat;
-  console.log("prices:");
-  console.log(priceWithVat, priceWithoutVat);
-  // TODO: process payment VAT
+  const productPrice = +data["ProdPrice"] - commissionPrice;
+
+  // Create the new invoice
+  const invoiceData = {
+    terminalnumber: data["terminalnumber"],
+    UserName: "test9611",
+    InvoiceType: "1",
+    InvoiceHead: {
+      CustName: data["CardOwnerName"],
+      CustAddresLine1: data["InvAddress"],
+      CustAddresLine2: data["InvAddress2"],
+      CustCity: data["intCity"],
+      CustLinePH: data["InvPhone"],
+      CustMobilePH: data["InvMobile"],
+      CompID: data["UID"],
+      Language: "he",
+      Comments: "Thanks for buying",
+      CoinID: "1",
+      Email: data["UserEmail"],
+      SendByEmail: "true",
+    },
+    InvoiceLines: [
+      { Description: "Product", Price: commissionPrice.toFixed(2), Quantity: 1, IsVatFree: "true" },
+      { Description: "Comission", Price: productPrice.toFixed(2), Quantity: 1, IsVatFree: "false" },
+    ],
+    "CreditDealNum.DealNumber": data["internaldealnumber"],
+  };
+
+  return invoiceData;
 };
+
+function buildQueryString(data) {
+  const params = new URLSearchParams();
+
+  for (const key in data) {
+    if (typeof data[key] === "object" && !Array.isArray(data[key])) {
+      for (const subKey in data[key]) {
+        params.append(`${key}.${subKey}`, data[key][subKey]);
+      }
+    } else if (Array.isArray(data[key])) {
+      data[key].forEach((item, index) => {
+        for (const subKey in item) {
+          params.append(`${key}${index + 1}.${subKey}`, item[subKey]);
+        }
+      });
+    } else {
+      params.append(key, data[key]);
+    }
+  }
+
+  return params.toString();
+}
 
 const createInvoice = async (invoiceData) => {
-  console.log("creating new invoice ");
-  const newInvoice = { terminalnumber: invoiceData["terminalnumber"] };
-  console.log(newInvoice);
-  // try {
-  //   const response = await axios.post("https://secure.cardcom.co.il/Interface/CreateInvoice.aspx", qs.stringify(invoiceData), {
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //     },
-  //   });
+  const queryString = buildQueryString(invoiceData);
+  const url = `https://secure.cardcom.co.il/Interface/CreateInvoice.aspx?${queryString}`;
 
-  //   const result = qs.parse(response.data);
+  console.log(url);
 
-  //   if (result.ResponseCode === "0") {
-  //     console.log(`Invoice Number: ${result.InvoiceNumber} Invoice Type: ${result.InvoiceType}`);
-  //   } else {
-  //     console.error("Error:", result);
-  //   }
-  // } catch (error) {
-  //   console.error("Error:", error);
-  // }
-};
+  try {
+    const result = await axios.post(url);
 
-const sendToPaymentService = async (data) => {
-  //   const paymentServiceUrl = process.env.PAYMENT_SERVICE_API_URL;
-  //   const apiKey = process.env.API_KEY;
-  //   try {
-  //     const response = await axios.post(paymentServiceUrl, data, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${apiKey}`,
-  //       },
-  //     });
-  //     console.log("Response from payment service:", response.data);
-  //   } catch (error) {
-  //     console.error("Error sending data to payment service:", error.response?.data || error.message);
-  //     throw error;
-  //   }
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 app.listen(PORT, () => {
