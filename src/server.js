845a9280dfdc80543e6d62d03b676f2353e5c705
Gil Kagan <gilkag2@gmail.com>
2024-07-02 17:10:52 +0300

    parsing response, invoice created successfully

--- src/server.js
+++ src/server.js
@@ -14,14 +14,19 @@ app.post("/", async (req, res) => {
   console.log("recieved data:");
   console.log(req.body);
 
+  let newInvoiceData;
   try {
-    const newInvoiceData = createNewInvoiceData(req.body);
+    newInvoiceData = createNewInvoiceData(req.body);
+  } catch (err) {
+    return res.status(404).send({ message: err.message });
+  }
+  try {
     await createInvoice(newInvoiceData);
 
     res.status(200).send("Webhook received and invoice created successfully");
   } catch (error) {
-    console.error("Error handling webhook:", error);
-    res.status(500).send("Internal Server Error");
+    console.error(error);
+    res.status(500).send(error.message || "Internal Server Error");
   }
 });
 
@@ -31,20 +36,27 @@ conversionMap.set("1", 50);
 conversionMap.set("2", 50);
 
 const createNewInvoiceData = (data) => {
-  console.log(`processing data for item ${data["ProdItemID"]}`);
-  const commissionPrice = conversionMap.get(data["ProdItemID"]) || 0; // maps product name to vat pricing information
-
-  const productPrice = +data["ProdPrice"] - commissionPrice;
-
-  const invoiceLines = [{ Description: "Product", Price: productPrice.toFixed(2), Quantity: 1, IsVatFree: "true" }];
-  if (commissionPrice > 0) {
-    invoiceLines.push({ Description: "Comission", Price: commissionPrice.toFixed(2), Quantity: 1, IsVatFree: "false" });
+  if (!data["ProdItemID"] || !data["ProdPrice"]) {
+    throw new Error("Invalid webhook data: missing 'ProdItemID' or 'ProdPrice'");
   }
+  console.log(`processing data for item ${data["ProdItemID"]}`);
+
+  let productPrice = +data["ProdPrice"];
+
+  const invoiceLines = [];
+
+  if (conversionMap.has(data["ProdItemID"])) {
+    const commissionPrice = conversionMap.get(data["ProdItemID"]);
+    invoiceLines.push({ Description: "Comission", Price: commissionPrice.toFixed(2), Quantity: 1, IsVatFree: "false" });
+    productPrice -= commissionPrice;
+  }
+
+  invoiceLines.push({ Description: "Product", Price: productPrice.toFixed(2), Quantity: 1, IsVatFree: "false" });
 
   // Create the new invoice
   const invoiceData = {
     terminalnumber: data["terminalnumber"],
-    UserName: "test9611",
+    UserName: "kzFKfohEvL6AOF8aMEJz",
     InvoiceType: "1",
     InvoiceHead: {
       CustName: data["CardOwnerName"],
@@ -67,7 +79,24 @@ const createNewInvoiceData = (data) => {
   return invoiceData;
 };
 
-function buildQueryString(data) {
+const createInvoice = async (invoiceData) => {
+  const queryString = buildQueryString(invoiceData);
+  const url = `https://secure.cardcom.co.il/Interface/CreateInvoice.aspx?${queryString}`;
+  try {
+    const result = await axios.post(url);
+    const parsedData = parseResponseData(result.data);
+    const { ResponseCode, Description } = parsedData;
+    if (ResponseCode !== 0) {
+      console.log(result.data);
+      throw new Error("Failed to create invoice");
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to contact card com services");
+  }
+};
+
+const buildQueryString = (data) => {
   const params = new URLSearchParams();
 
   for (const key in data) {
@@ -87,18 +116,15 @@ function buildQueryString(data) {
   }
 
   return params.toString();
-}
+};
 
-const createInvoice = async (invoiceData) => {
-  const queryString = buildQueryString(invoiceData);
-  const url = `https://secure.cardcom.co.il/Interface/CreateInvoice.aspx?${queryString}`;
-  try {
-    const result = await axios.post(url);
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-    throw new Error("Failed to create invoice");
+const parseResponseData = (responseData) => {
+  const params = new URLSearchParams(responseData);
+  const parsedData = {};
+  for (const [key, value] of params) {
+    parsedData[key] = value;
   }
+  return parsedData;
 };
 
 app.listen(PORT, () => {
